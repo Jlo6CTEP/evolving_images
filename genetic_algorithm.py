5f9a78abeab1a8cd8a5b0bfa077750c3cca7c6d4
Jlo6CTEP <35602222+Jlo6CTEP@users.noreply.github.com>
2019-04-06 18:34:39 +0300

    algorithm works

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,17 +1,20 @@
+import logging
+import os
 import time
 from copy import deepcopy
 
 import numpy
 
-from numpy import argwhere, zeros, uint8, full, arange, concatenate, empty, ndarray, array, clip, int32, int8, insert, \
-    argsort
+from numpy import argwhere, zeros, uint8, full, arange, concatenate, empty, ndarray, array, clip, int32, int8, int16, \
+    save, load
 from numpy.random import exponential, randint, normal, choice
 from scipy.spatial import Delaunay
 from scipy.spatial import distance
+from multiprocessing import Pool, get_logger, log_to_stderr
 import cv2
 
 SIZE = 512
-POPULATION_SIZE = 100
+POPULATION_SIZE = 30
 F_SIZE = 64
 
 KERNEL_SIZE = 3
@@ -22,7 +25,7 @@ EDGE_COUNT_PENALTY = 8
 
 MAX_POINT_FACTOR = 1 / 8
 MIN_POINT_COUNT = 4
-COLOR_DRIFT = 5
+COLOR_DRIFT = 3
 POINT_DRIFT = F_SIZE / 16
 RANDOM_POINTS = 3
 FITNESS_STEP = 2
@@ -53,7 +56,7 @@ def generate_population(source, mask):
     border_points_bottom_right[F_SIZE:2 * F_SIZE, 0] = arange(0, F_SIZE)
     border_points = concatenate((border_points_top_left, border_points_bottom_right))
 
-    population = empty(POPULATION_SIZE, ndarray)
+    population = empty([POPULATION_SIZE, 4], ndarray)
 
     for n in range(POPULATION_SIZE):
         inner_count += randint(-RANDOM_POINTS, RANDOM_POINTS)
@@ -93,11 +96,12 @@ def triangulate(mesh):
 
 
 def calculate_colors(mesh, triangles, src, drift):
-    colors = empty([triangles.shape[0], 3], dtype=uint8)
+    colors = empty([triangles.shape[0], 3], dtype=int16)
     for x in range(triangles.shape[0]):
         cm = array(numpy.sum(mesh[triangles[x]], axis=0) / 3, dtype=int32)
         colors[x] = src[cm[1], cm[0]].tolist() + drift
-    return colors
+    colors = clip(colors, 0, 255)
+    return colors.astype(uint8)
 
 
 def draw_square(mesh, triangles, colors, fitness):
@@ -113,7 +117,6 @@ def crossover(ind_one, ind_two, src):
 
     crossover_mode = int(clip(int(exponential(1 / MODE_LAMBDA)), 0, 3))
     crossover_intensity = int(clip(int(exponential(1 / INTENSITY_LAMBDA) * 2), 1, 4))
-    print("crossover mode is {}".format(crossover_mode))
     if crossover_mode == 0:
         exchange_colors(child_one, child_two, crossover_intensity)
     elif crossover_mode == 1:
@@ -123,8 +126,6 @@ def crossover(ind_one, ind_two, src):
         exchange_points(child_one, child_two, src, crossover_intensity)
     child_one[3] = evaluate_fitness(child_one, src)
     child_two[3] = evaluate_fitness(child_two, src)
-    print(child_one[3])
-    print(child_two[3])
     return child_one, child_two
 
 
@@ -135,10 +136,11 @@ def exchange_colors(ind_one, ind_two, crossover_intensity):
 
 def exchange_points(ind_one, ind_two, src, crossover_intensity):
     shorter, longer = (ind_one[0], ind_two[0]) if len(ind_one[0]) < len(ind_two[0]) else (ind_two[0], ind_one[0])
-    shorter = concatenate([empty([len(longer) - len(shorter), 2], dtype=ndarray), shorter])
+    shorter = concatenate([full([len(longer) - len(shorter), 2], -1, dtype=int8), shorter])
     loci = choice(len(shorter), crossover_intensity, replace=False)
     shorter[loci], longer[loci] = longer[loci], shorter[loci]
-    shorter = shorter[(shorter != array([None, None]))[:, 0]]
+    shorter = shorter[(shorter != array([-1, -1]))[:, 0]]
+    longer = longer[(longer != array([-1, -1]))[:, 0]]
     ind_one[0], ind_two[0] = (shorter, longer) if len(ind_one[0]) < len(ind_two[0]) else (longer, shorter)
 
     ind_one[0] = ndarray.astype(ind_one[0], dtype=int32)
@@ -177,26 +179,30 @@ def mutation(ind, src):
     mutated[3] = evaluate_fitness(mutated, src)
     return mutated
 
+
 def mutate_colors(ind, mutation_intensity):
-    loci = choice(len(ind[2]) - 1, mutation_intensity, False)
+    mutation_intensity = min(len(ind[2]), mutation_intensity)
+    loci = choice(len(ind[2]), mutation_intensity, False)
+    ind[2][loci] = ind[2][loci].astype(dtype=int16)
     ind[2][loci] = ind[2][loci] + \
-                   ndarray.astype(clip((normal(0, 1, [mutation_intensity, 3]) * 2), -COLOR_DRIFT, COLOR_DRIFT),
-                                  dtype=int8)
-    clip(ind[2], 0, 255)
+                   clip((normal(0, 1, [mutation_intensity, 3]) * 2), -COLOR_DRIFT, COLOR_DRIFT).astype(dtype=int8)
+    ind[2] = clip(ind[2], 0, 255)
 
 
 def mutate_point_position(ind, mutation_intensity, src):
-    loci = choice(len(ind[0]) - 1, mutation_intensity, False)
+    mutation_intensity = min(len(ind[0]), mutation_intensity)
+    loci = choice(len(ind[0]), mutation_intensity, False)
     ind[0][loci] = ind[0][loci] + \
                    ndarray.astype(clip((normal(0, 2, [mutation_intensity, 2]) * 2), -POINT_DRIFT, POINT_DRIFT),
                                   dtype=int8)
+    ind[0][loci] = clip(ind[0][loci], 0, F_SIZE - 1)
     ind[1] = triangulate(ind[0])
     ind[2] = calculate_colors(ind[0], ind[1], src, zeros([3]))
 
 
 def del_add_mutation(ind, mutation_intensity, src):
     is_del = randint(0, 2)
-    if is_del == 1:
+    if is_del == 1 and len(ind[2]) - mutation_intensity > 4:
         loci = choice(len(ind[0] - 5), len(ind[0]) - mutation_intensity, False)
         ind[0] = ind[0][loci]
     else:
@@ -205,28 +211,32 @@ def del_add_mutation(ind, mutation_intensity, src):
     ind[2] = calculate_colors(ind[0], ind[1], src, zeros([3]))
 
 
-def approximate_square(source, mask):
+def approximate_square(source, mask, x, y):
     population = array(generate_population(source, mask))
-    population = population[argsort(population, 3)]
+    population = population[population[:, 3].argsort()[::-1]]
     fitness = population[POPULATION_SIZE - 1][3]
-    while fitness > 30:
-        breeding = population[POPULATION_SIZE - CROSSOVER_COUNT, POPULATION_SIZE - 1]
+    counter = 0
+    print("Generation {}, fitness {}, thread {}, point {}, {}".format(counter, fitness, os.getpid(), x ,y))
+    while fitness > 400:
+        counter += 1
+        breeding = population[POPULATION_SIZE - CROSSOVER_COUNT: POPULATION_SIZE - 1]
 
-        for x in range(len(breeding) - 1, step=2):
-            breeding[x], breeding[x + 1] = crossover(breeding[x], breeding[x + 1], source)
+        for f in range(0, len(breeding) - 1, 2):
+            breeding[f], breeding[f + 1] = crossover(breeding[f], breeding[f + 1], source)
 
         population = concatenate([breeding, population])
 
         mutating = choice(len(population) - 1, MUTATION_COUNT)
 
-        for x in range(len(mutating) - 1):
-            population[x] = mutation(population[x], source)
+        for f in range(len(mutating) - 1):
+            population[f] = mutation(population[f], source)
 
-        population = population[argsort(population, 3)]
+        population = population[population[:, 3].argsort()[::-1]]
         population = population[len(population) - POPULATION_SIZE:]
         fitness = population[POPULATION_SIZE - 1][3]
+        print("Generation {}, fitness {}, thread {}, point {}, {}".format(counter, fitness, os.getpid(), x ,y))
     super_hero = population[POPULATION_SIZE - 1]
-    return draw_square(*super_hero)
+    save("./pic/square_{}_{}".format(x, y), draw_square(*super_hero))
 
 
 def evaluate_fitness(tgt, src):
@@ -238,20 +248,43 @@ def evaluate_fitness(tgt, src):
     return fit / (F_SIZE ** 2)
 
 
-img = cv2.resize(cv2.imread(PICTURE), (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-# gray = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (KERNEL_SIZE, KERNEL_SIZE), 0)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if __name__ == '__main__':
+    img = cv2.resize(cv2.imread(PICTURE), (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+    # gray = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (KERNEL_SIZE, KERNEL_SIZE), 0)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret, laplacian = cv2.threshold(cv2.Laplacian(gray, cv2.CV_8U), THRESHOLD, MAX_BLACK, cv2.THRESH_BINARY)
-sobely = cv2.Sobel(gray, cv2.CV_8U, 1, 1, ksize=5)
+    ret, laplacian = cv2.threshold(cv2.Laplacian(gray, cv2.CV_8U), THRESHOLD, MAX_BLACK, cv2.THRESH_BINARY)
+    sobely = cv2.Sobel(gray, cv2.CV_8U, 1, 1, ksize=5)
 
-cv2.imshow('init', img)
+    t1 = time.time()
+    img2 = empty([SIZE, SIZE, 3], dtype=uint8)
 
-t1 = time.time()
-f = approximate_square(img[0:F_SIZE, 0:F_SIZE], laplacian[0:F_SIZE, 0:F_SIZE])
-cv2.imshow("first_square", f)
-print(time.time() - t1)
-cv2.imshow('laplacian', laplacian)
-cv2.imshow('sobely', sobely)
+    log_to_stderr()
 
-cv2.waitKey()
+    logger = get_logger()
+    logger.setLevel(logging.INFO)
+
+    # lock = Lock()
+
+    with Pool(8) as p:
+        time.sleep(1)
+        for x in range(int(SIZE / F_SIZE)):
+            for y in range(int(SIZE / F_SIZE)):
+                a = p.apply_async(approximate_square,
+                                  (img[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE],
+                                   laplacian[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE], x, y))
+        p.close()
+        p.join()
+
+    for x in range(int(SIZE / F_SIZE)):
+        for y in range(int(SIZE / F_SIZE)):
+            img2[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE] = load(
+                "./pic/square_{}_{}.npy".format(x, y))
+
+    cv2.imshow("first_img", img)
+    cv2.imshow("second_img", img2)
+    print(time.time() - t1)
+    cv2.imshow('laplacian', laplacian)
+    cv2.imshow('sobely', sobely)
+
+    cv2.waitKey()
