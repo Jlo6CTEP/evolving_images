8f6aae5aa59bb95450e07cd947206050d49354e4
Jlo6CTEP <19yura99@mail.ru>
2019-04-07 20:59:07 +0300

    alternative method

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,16 +1,6 @@
-import os
 import time
-from copy import deepcopy
 
-import numpy
-
-from numpy import argwhere, zeros, uint8, full, arange, concatenate, empty, ndarray, array, clip, int32, int8, int16, \
-    save, load
-from numpy.random import exponential, randint, normal, choice
-from scipy.spatial import Delaunay
-from scipy.spatial import distance
-from multiprocessing import Pool
-import cv2
+from numpy import load
 
 from evaluator import *
 
@@ -19,7 +9,7 @@ PICTURE = "./picture/Tower2.jpg"
 
 if __name__ == '__main__':
     img = cv2.resize(cv2.imread(PICTURE), (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-    # gray = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (KERNEL_SIZE, KERNEL_SIZE), 0)
+    # gray = cv2.GaussianBlur(cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY), (KERNEL_SIZE, KERNEL_SIZE), 0)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     ret, laplacian = cv2.threshold(cv2.Laplacian(gray, cv2.CV_8U), BLACK_THRESHOLD, MAX_BLACK, cv2.THRESH_BINARY)
@@ -28,15 +18,15 @@ if __name__ == '__main__':
     t1 = time.time()
     img2 = empty([SIZE, SIZE, 3], dtype=uint8)
 
-    with Pool(8, maxtasksperchild=1) as p:
-        time.sleep(1)
-        for x in range(0, 4):
-            for y in range(int(SIZE / F_SIZE)):
-                a = p.apply_async(approximate_square,
-                                  (img[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE],
-                                   laplacian[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE], x, y))
-        p.close()
-        p.join()
+    #with Pool(8, maxtasksperchild=1) as p:
+    #    time.sleep(1)
+    #    for x in range(4, int(SIZE / F_SIZE)):
+    #        for y in range(int(SIZE / F_SIZE)):
+    #            a = p.apply_async(approximate_square,
+    #                              (img_src[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE],
+    #                               laplacian[x * F_SIZE: (x + 1) * F_SIZE, y * F_SIZE: (y + 1) * F_SIZE], x, y))
+    #    p.close()
+    #    p.join()
 
     for x in range(int(SIZE / F_SIZE)):
         for y in range(int(SIZE / F_SIZE)):
---
+++ genetics2.py
@@ -0,0 +1,163 @@
+import time
+from copy import deepcopy
+from multiprocessing import Process
+from multiprocessing.pool import Pool
+
+import cv2
+import numpy as np
+from numpy import argwhere, zeros, full, arange, concatenate, ndarray, array, clip, int32, int8, int16, save, empty, \
+    uint8, ceil
+
+from numpy.random import exponential, randint, normal, choice
+from numba import jit
+from numba.types import uintp
+
+PICTURE = "./picture/Tower2.jpg"
+SIZE = 512
+
+COLOR_DRIFT = 30
+POINT_DRIFT = 40
+
+INTENSITY_LAMBDA = 0.5
+INTENSITY_MULT = 4
+
+POPULATION_SIZE = 40
+RADIUS = 4
+
+SQUARE_SIZE = int(ceil(RADIUS * 2 ** 1 / 2))
+
+DOT_COUNT = 40000
+ITERATION_COUNT = 10
+MUTATION_COUNT = int(POPULATION_SIZE / 3)
+CROSSOVER_COUNT = int(POPULATION_SIZE / 2)
+
+
+@jit()
+def generate_population(src):
+    population = empty([POPULATION_SIZE, 3], dtype=ndarray)
+    for x in range(POPULATION_SIZE):
+        ind = empty([3], dtype=ndarray)
+        ind[0] = randint(SQUARE_SIZE, SQUARE_SIZE + SIZE, 2)
+        ind[1] = randint(0, 255, 3)
+        ind[2] = fitness(ind, src)
+        population[x] = ind
+    return population
+
+
+@jit()
+def draw_individual(picture, individual):
+    cv2.circle(picture, tuple(individual[0])[::-1], RADIUS, individual[1].tolist(), thickness=cv2.FILLED)
+
+
+@jit()
+def fitness(individual, src):
+    test_square = full([SQUARE_SIZE, SQUARE_SIZE, 3], individual[1], dtype=ndarray)
+    shift = int(ceil(SQUARE_SIZE / 2))
+    diff = src[individual[0][0] - shift: individual[0][0] + shift,
+           individual[0][1] - shift: individual[0][1] + shift] - test_square
+    return np.sum(np.sum(np.abs(diff)))
+
+
+@jit()
+def mutate(ind, src):
+    mutated = deepcopy(ind)
+    mutation_mode = randint(0, 3)
+    if mutation_mode == 0:
+        mutate_color(mutated)
+    elif mutation_mode == 1:
+        mutate_point(mutated)
+    else:
+        mutate_color(mutated)
+        mutate_point(mutated)
+    mutated[2] = fitness(mutated, src)
+    return mutated
+
+
+@jit()
+def mutate_color(ind):
+    ind[1] = ind[1].astype(dtype=int16)
+    ind[1] = ind[1] + \
+             clip((normal(0, 1, [3]) * 8), -COLOR_DRIFT, COLOR_DRIFT).astype(dtype=int8)
+    ind[1] = clip(ind[1], 0, 255)
+    ind[1] = ind[1].astype(dtype=uint8)
+    return ind
+
+
+@jit()
+def mutate_point(ind):
+    ind[0] = ind[0] + \
+             ndarray.astype(clip((normal(0, 2, [2]) * 8), -POINT_DRIFT, POINT_DRIFT), dtype=int16)
+    ind[0] = clip(ind[0], SQUARE_SIZE, SQUARE_SIZE + SIZE)
+    return ind
+
+
+@jit()
+def crossover(ind_one, ind_two, src):
+    child_one = deepcopy(ind_one)
+    child_two = deepcopy(ind_two)
+    crossover_mode = randint(0, 3)
+    crossover_intensity = randint(0, 2)
+    if crossover_mode == 0:
+        cross_colors(child_one, child_two)
+    elif crossover_mode == 1:
+        cross_points(child_one, child_two, crossover_intensity)
+    else:
+        cross_colors(child_one, child_two)
+        cross_points(child_one, child_two, crossover_intensity)
+    child_one[2] = fitness(child_one, src)
+    child_two[2] = fitness(child_two, src)
+    return child_one, child_two
+
+
+@jit()
+def cross_colors(ind_one, ind_two):
+    ind_one[1], ind_two[1] = ind_two[1], ind_one[1]
+
+
+@jit()
+def cross_points(ind_one, ind_two, intensity):
+    if intensity == 0:
+        mode = randint(0, 2)
+        if mode == 0:
+            ind_one[0][0], ind_two[0][0] = ind_two[0][0], ind_one[0][0]
+        else:
+            ind_one[0][1], ind_two[0][1] = ind_two[0][1], ind_one[0][1]
+    else:
+        ind_one[0], ind_two[0] = ind_two[0], ind_one[0]
+
+
+@jit()
+def evaluate(src, tgt, number):
+    print("Process #{} started".format(number))
+    counter = 0
+    for x in range(int(DOT_COUNT)):
+        print(x)
+        counter += 1
+        pop = generate_population(src)
+        pop = pop[pop[:, 2].argsort()[::-1]]
+        for y in range(ITERATION_COUNT):
+            breeding = pop[POPULATION_SIZE - CROSSOVER_COUNT: POPULATION_SIZE]
+            for f in range(0, len(breeding) - 1, 2):
+                pop[f], pop[f + 1] = crossover(breeding[f], breeding[f + 1], src)
+            mutating = choice(len(pop) - 1, MUTATION_COUNT)
+            for f in range(len(mutating) - 1):
+                pop[f] = mutate(pop[f], src)
+            pop = pop[pop[:, 2].argsort()[::-1]]
+        draw_individual(tgt, pop[POPULATION_SIZE - 1])
+
+
+if __name__ == '__main__':
+    img_src = zeros([SIZE + 2 * SQUARE_SIZE, SIZE + 2 * SQUARE_SIZE, 3], dtype=uint8)
+    img_src[SQUARE_SIZE: SQUARE_SIZE + SIZE, SQUARE_SIZE: SQUARE_SIZE + SIZE] = \
+        cv2.resize(cv2.imread(PICTURE), (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+
+    dst = zeros([SIZE + SQUARE_SIZE, SIZE + SQUARE_SIZE, 3], dtype=uint8)
+
+    t1 = time.time()
+
+    cv2.imshow("source", img_src)
+    cv2.imshow("result", dst)
+    cv2.imwrite("tor gvalchca.png", dst)
+    print("Work time is {}".format(time.time() - t1))
+    cv2.waitKey()
+
